fd47a8f00b9b9abe797dd7cacac21ebe4fc2b491
kennliang <liangkenny65@gmail.com>
2019-11-16 18:34:10 -0500

    testing micro

--- media_app/routes/index.js
+++ media_app/routes/index.js
@@ -19,6 +19,8 @@ client.connect(function(err){
   console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
 });
 
+var db = require('../app.js');
+
 router.post('/addmedia',upload.single('content'),function(req,res,next){
   const addMedia = async function(req,res) {
     try
@@ -66,6 +68,91 @@ router.get('/media/:id',function(req,res,next){
   get_media(req,res);
 });
 
-var db = require('../app.js');
+
+router.get('/search',function(req,res,next){
+  res.render('search');
+});
+
+router.post('/search',function(req,res,next){
+  const search = async function(req,res){
+    try
+    {
+      const {timestamp,limit,username,following,q,hasMedia,rank,parent,replies} = req.body;
+      console.log("the parameters of search is ",timestamp,limit,username,following,q,hasMedia,rank,parent,replies);
+      let search_limit = 25;
+      let search_time = Date.now()/1000;
+
+      if(timestamp != null)
+        search_time = timestamp;
+
+      if(limit != null && limit > 0 && limit <= 100)
+        search_limit = limit;
+
+      if(limit > 100)
+        search_limit = 100;
+
+      let query_array = [{timestamp: {$lte: search_time}}];
+
+      if(username != null && username != '')
+        query_array.push({username : username});
+      if(q != null && q != '' && !(/^\s+$/.test(q)))
+        query_array.push({$text: {$search: q}});
+
+      let has_media_query = false;
+      if(hasMedia != null)
+        has_media_query = hasMedia;
+      if(has_media_query)
+        query_array.push({media:{ $exists: true, $ne: [] }});
+
+      //get followers of current signed in user if following is true
+      if(req.session.username != null && following == true){
+        console.log("accessed with user logged in and following ture");
+        let result = db.user.findOne({username:req.session.username});
+        if(result == null)
+          throw new Error("Unable to find current logged in username in database at /search ")
+       
+        let followers_array = result.following;
+        let followers_query = [];
+        for(let i = 0; i < followers_array.length; i++)
+          followers_query.push({username: followers_array[i]});
+        if(followers_query.length != 0)
+          query_array.push({$or: followers_query});
+      }
+      if(replies == false)
+        query_array.push({replies: {$not :{$eq: "reply"}}});
+      else if(replies == null || replies == true)
+      {
+        if(parent != null && parent != '')
+        {
+          //console.log("executed3wedsaweds");
+          console.log(parent);
+          query_array.push({parent:parent});
+          //query_array.push({$and : [{parent : parent},{$or :[{child_Type: {$eq: "retweet"}},{child_Type:{ $eq:"reply"}}]}]  }  );
+        }
+      }
+
+      let sorter = {total: -1};
+      if(rank == "time")
+        sorter = {timestamp: -1};
+      let query = {$and :query_array};
+      console.log(query_array);
+      let result = await db.post.find(query).sort(sorter).limit(search_limit).toArray();
+      if(result == null)
+        throw new Error("Unable to perform search");
+      console.log(result);
+      res.status(200).send({"status": "OK","items":result});
+    
+    }
+    catch(e){
+      console.log(e);
+      res.status(500).send({"status":"error","error": e});
+    }
+  }
+  search(req,res);
+});
+
+
+
+//var db = require('../app.js');
 
 module.exports = router;
--- users_app/routes/index.js
+++ users_app/routes/index.js
@@ -6,4 +6,119 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/item/:id',function(req,res,next){
+  let get_item = async function(req,res){
+    try
+    {
+      let item_id = req.params.id;
+      //console.log("Get item with id " + item_id);
+      let result = await db.post.findOne({id : item_id});
+      if(result == null)
+        throw new Error("Unable to find the post with id "+ item_id);
+      res.status(200).send({"status": "OK","item":result});
+    }
+    catch(e){
+      console.log(e);
+      res.status(500).send({"status":"error","error": e});
+    }
+  } 
+  get_item(req,res);
+});
+
+router.delete('/item/:id',function(req,res,next){
+  const delete_item = async function(req,res){
+    try
+    {
+      let item_id = req.params.id;
+      if(req.session.username == null)
+        throw new Error("User is not logged in to delete item");
+      
+      //console.log("Deleting item with id " + item_id);
+      let result = await db.post.findOneAndDelete({username: req.session.username,id:item_id});
+     
+      if(result == null || result.value == null)
+        throw new Error("Unable to find or delete the post with id "+ item_id);
+
+    
+      let child_type = result.value.childType;
+      const options = {upsert:false};
+      if(child_type == "retweet")
+      {
+        const parent_query = {id: result.value.parent};
+        const update_info = {$inc :{retweeted: -1}};
+        let update_retweet = await db.post.updateOne(parent_query,update_info,options);
+        if(update_retweet == null || update_retweet.matchedCount == 0 || update_retweet.modifiedCount == 0)
+          throw new Error("Unable to find or update the parent post retweet count");
+      }
+
+      let media_array = result.value.media;
+      if(media_array != null)
+      {
+        for(let i = 0; i < media_array.length; i++)
+        {
+          let query = 'DELETE FROM Media WHERE id = ?';
+          await client.execute(query,[media_array[i]],{prepare:true});
+        }
+      }
+      const query = { username: result.value.username};
+      const update = { $pull: {posts: result.value.id}};
+     
+      let result2 = await db.user.updateOne(query,update,options);
+      if(result2 == null || result2.matchedCount == 0 || result2.modifiedCount == 0)
+        throw new Error("Unable to find and delete the post from the user");
+      res.status(200).send({"status": "OK"});
+    }
+    catch(e){
+      console.log(e);
+      res.status(500).send({"status":"error","error": e});
+    }
+  }
+  delete_item(req,res);
+});
+
+
+router.post('/item/:id/like',function(req,res,next){
+  const like = async function(req,res){
+    try
+    {
+      const {username} = req.session;
+      const {like} = req.body;
+      const {id} = req.params;
+      if(username == null)
+        throw new Error("User is not logged in to like a post");
+  
+        //let item_id = req.params.id;
+        let like_status = true;
+        if(like != null)
+          like_status = like;
+
+        const query = { id: id};
+        const update_options = {upsert:false,returnOriginal: false};
+        const options = {upsert:false};
+
+        let update_info;
+        if(like_status)
+          update_info = { $addToSet:{likes: username}};
+        else
+          update_info = { $pull: {likes: username}};
+
+        let result = await db.post.findOneAndUpdate(query,update_info,update_options);
+        if(result == null)
+          throw new Error("Unable to find and update item being liked with id " + id);
+        update_info = {$set:{"property.likes":result.value.likes.length,"total": result.value.likes.length + result.value.retweeted}};
+        let result2 = await db.post.updateOne(query,update_info,options);
+        if(result2 == null || result2.matchedCount == 0 || result2.modifiedCount == 0)
+          throw new Error("Unable to update the item being liked with id " + id)
+        res.status(200).send({"status":"OK"});
+    }
+    catch(e){
+      console.log("Try catch error at like "+ e);
+      res.status(500).send({"status":"error","error":"Error thrown at like"+e});
+    }
+  }
+  like(req,res);
+
+});
+
+
 module.exports = router;
