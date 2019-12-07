e077f9e2a14c01e5cd75445476bc78830be7d21b
kennliang <liangkenny65@gmail.com>
2019-12-06 21:05:42 -0500

    ip changes

--- search_app/elasticsearch_test.js
+++ search_app/elasticsearch_test.js
@@ -4,10 +4,10 @@ const client = new Client({ node: 'http://152.44.37.110:9200' });
 
 async function run () {
 
-
+/*
     await client.indices.delete({
         index: 'game'});
-        
+        */
     await client.indices.create({
         index: 'game'
     });
